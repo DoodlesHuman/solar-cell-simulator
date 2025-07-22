6ae32120f877619ec6f0293d243fc7c23f29adad
DoodlesHuman <amosrr@gmail.com>
2025-07-22 13:56:44 +0200

    Updated sim

--- src/simulation.py
+++ src/simulation.py
@@ -1,3 +1,5 @@
+from src.utils import timeit, log_step
+
 class SolarCellSim:
     def __init__(self, carriers, field, dt = 0.01, steps = 100):
         self.carriers = carriers
@@ -6,6 +8,20 @@ class SolarCellSim:
         self.steps = steps
         self.trajectory = []
 
+@timeit
+def run(self):
+    for _ in range(self.steps):
+        self.run_step()
+
+@log_step
+def run_step(self):
+    positions = []
+    for c in self.carriers:
+        c.move(self.field, self.dt)
+        positions.append((c.charge_type, c.position))
+    self.trajectory.append(positions)
+
+"""
     def run(self):
         for _ in range(self.steps):
             step_data = []
@@ -13,3 +29,4 @@ class SolarCellSim:
                 c.move(self.field, self.dt)
                 step_data.append(c.position)
             self.trajectory.append(step_data)
+"""
